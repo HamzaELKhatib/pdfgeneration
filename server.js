b25489934aa3ec5120c6472a58a0b27d496f4ba5
HamzaELKhatib <contact@hamzaelkhatib.com>
2024-09-01 19:14:31 +0100

    Created a route to generate a pdf through an api

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const Koa = require('koa');
+const Router = require('@koa/router');
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const app = new Koa();
+const router = new Router();
 
-app.use(async ctx => {
+router.get('/generatePdf', async ctx => {
     try {
-        console.log('Received request');
+        const url = ctx.query.url;
+        if (!url) {
+            ctx.status = 400;
+            ctx.body = 'URL parameter is required';
+            return;
+        }
+
+        console.log(`Received request to generate PDF for: ${url}`);
         const browser = await puppeteer.launch({
             headless: true,
             args: ['--no-sandbox', '--disable-setuid-sandbox']
@@ -13,7 +22,7 @@ app.use(async ctx => {
         console.log('Browser launched');
         const page = await browser.newPage();
         console.log('New page created');
-        await page.goto('https://www.odi.ch/prog/design/newbies.php', { waitUntil: 'networkidle2' });
+        await page.goto(url, {waitUntil: 'networkidle2'});
         console.log('Page loaded');
 
         const pdfBuffer = await page.pdf({
@@ -29,7 +38,8 @@ app.use(async ctx => {
         console.log('PDF generated');
 
         // Save PDF to local filesystem
-        fs.writeFileSync('webpage.pdf', pdfBuffer);
+        const filePath = 'webpage.pdf';
+        fs.writeFileSync(filePath, pdfBuffer);
         console.log('PDF saved locally');
 
         await browser.close();
@@ -38,7 +48,7 @@ app.use(async ctx => {
         // Stream the PDF to the client
         ctx.set('Content-Type', 'application/pdf');
         ctx.set('Content-Disposition', 'attachment; filename=webpage.pdf');
-        ctx.body = fs.createReadStream('webpage.pdf');
+        ctx.body = fs.createReadStream(filePath);
         console.log('Response sent');
     } catch (error) {
         console.error('Error generating PDF:', error);
@@ -46,6 +56,10 @@ app.use(async ctx => {
         ctx.body = 'Internal Server Error';
     }
 });
+
+app
+    .use(router.routes())
+    .use(router.allowedMethods());
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
